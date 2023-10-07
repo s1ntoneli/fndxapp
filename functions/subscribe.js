c5aa972d17e7206d9b96337c0982cd909e1b0379
auv1107 <auv1107@gmail.com>
2023-10-07 13:02:22 +0800

    重写 functions
    Signed-off-by: auv1107 <auv1107@gmail.com>

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -1,41 +1,41 @@
+async function fetch(request, env) {
+    const defaultData = { todos: [] };
 
-export default {
-    async fetch(request, env) {
-      const defaultData = { todos: [] };
-  
-      const setCache = (key, data) => env.FNDX_NEWSLETTER.put(key, data);
-      const getCache = key => env.FNDX_NEWSLETTER.get(key);
-  
-      async function subscribe(request) {
-        const url = new URL(request.url);
-        console.log(url)
-        
-        const searchParams = url.searchParams
-        const mail = searchParams.get('mail');
-        console.log(mail);
-  
-        await setCache(mail, JSON.stringify({
-          subscribe: true,
-          timestamp: new Date().getTime()
-        }))
-        
-        return new Response(JSON.stringify({
-          code: 0,
-          message: "Success"
-        }), { status: 200, headers: { 'Content-Type': 'application/json'} });
-      }
-  
+    const setCache = (key, data) => env.FNDX_NEWSLETTER.put(key, data);
+    const getCache = key => env.FNDX_NEWSLETTER.get(key);
+
+    async function subscribe(request) {
       const url = new URL(request.url);
       console.log(url)
       
-      const pathname = url.pathname
-      const paths = pathname.split('/')
-      console.log('pathname', pathname, 'paths', paths)
-  
-    //   if (paths.length > 1 && paths[1] == 'subscribe') {
-        return await subscribe(request)
-    //   }
-    //   return new Response("Error", { status: 500 });
+      const searchParams = url.searchParams
+      const mail = searchParams.get('mail');
+      console.log(mail);
+
+      await setCache(mail, JSON.stringify({
+        subscribe: true,
+        timestamp: new Date().getTime()
+      }))
+      
+      return new Response(JSON.stringify({
+        code: 0,
+        message: "Success"
+      }), { status: 200, headers: { 'Content-Type': 'application/json'} });
     }
-  };
-  
+
+    const url = new URL(request.url);
+    console.log(url)
+    
+    const pathname = url.pathname
+    const paths = pathname.split('/')
+    console.log('pathname', pathname, 'paths', paths)
+
+  //   if (paths.length > 1 && paths[1] == 'subscribe') {
+      return await subscribe(request)
+  //   }
+  //   return new Response("Error", { status: 500 });
+}
+
+export async function onRequest(context) {
+    return fetch(context.request, context.env)
+}
